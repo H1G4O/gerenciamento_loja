6d1686da921be2edf40a7c27c135abe4221c6866
h1g4o <higor434@hotmail.com>
2025-10-31 19:27:10 -0300

    Add funcoes e forms

---
+++ main.js
@@ -0,0 +1,7 @@
+function open_form(formId){
+    document.getElementById(formId).style.display = "block";
+}
+
+function close_form(formId){
+    document.getElementById(formId).style.display = "none";
+}
