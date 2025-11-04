1cc25ee190ec7ca7b8699201ac466a5cb4814c57
h1g4o <higor434@hotmail.com>
2025-11-04 01:56:33 -0300

    Testando funcionalidades nao funcionou

--- main.js
+++ main.js
@@ -5,3 +5,16 @@ function open_form(formId){
 function close_form(formId){
     document.getElementById(formId).style.display = "none";
 }
+
+
+
+const cadastro_produto = document.querySelector('#cadastro_produto_form');
+
+cadastro_produto.addEventListener('submit', event =>{
+     event.preventDefault();
+
+     const formData = new FormData(cadastro_produto);
+     const data = Object.fromEntries(formData);
+
+     console.log(data);
+})
